99f89276ced6f6d61a119c1f0936654b17d4bcc2
tzchrzanowski <tzchrzanowski@gmail.com>
2023-03-03 20:06:41 +0800

    added few inner windows

--- src/pages/Scene.tsx
+++ src/pages/Scene.tsx
@@ -13,6 +13,7 @@ export const Scene = () => {
                 shadow-mapSize-width={2048}
                 shadow-mapSize-height={2048}
             />
+            {/*outer walls:*/}
             <Wall position={[0, 0, 5.5]} wallWidth={0.2} wallHeight={2.2} wallLength={11} props={""} />
             <Wall position={[5, 0, 5.5]} wallWidth={0.2} wallHeight={2.2} wallLength={11} props={""} />
             <Wall position={[1, 0, 11]} wallWidth={2} wallHeight={2.2} wallLength={0.2} props={""} />
@@ -21,6 +22,14 @@ export const Scene = () => {
             <Wall position={[0.5, 0, 0]} wallWidth={1} wallHeight={2.2} wallLength={0.2} />
             <Wall position={[3.5, 0, 0]} wallWidth={3} wallHeight={2.2} wallLength={0.2} />
             <Window position={[1.5, 0, 0]} glassWidth={1} />
+            {/* inner walls groundFloor:*/}
+            <Window position={[0.5, 0, 8]} glassWidth={1} />
+            <Pillar position={[1, 0, 8]} />
+            <Window position={[1.5, 0, 8]} glassWidth={1} />
+            <Pillar position={[2, 0, 8]} />
+            <Window position={[2.5, 0, 8]} glassWidth={1} />
+            <Pillar position={[3, 0, 8]} />
+            <Window position={[3.5, 0, 8]} glassWidth={1} />
         </>
     );
 };
