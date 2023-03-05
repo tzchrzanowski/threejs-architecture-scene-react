a92814915a65e3ff7a85edd2bcc78f0dd67a5f92
tzchrzanowski <tzchrzanowski@gmail.com>
2023-03-05 19:50:46 +0800

    grouped up structures into sections

--- src/pages/Scene.tsx
+++ src/pages/Scene.tsx
@@ -1,12 +1,13 @@
 import { useFrame } from '@react-three/fiber';
-import Pillar from "../walls/Pillar";
-import Wall from "../walls/Wall";
-import Window from "../walls/Window";
+
 import Stairs from "../combinedStructures/Stairs";
+import GlassWall from "../combinedStructures/GlassWall";
+import OuterWallsGround from "../combinedStructures/OuterWallsGround";
 
 export const Scene = () => {
     return (
         <>
+            {/* Scene setup: */}
             <ambientLight intensity={0.1}/>
             <pointLight
                 position={[10, 10, 10]}
@@ -14,24 +15,9 @@ export const Scene = () => {
                 shadow-mapSize-width={2048}
                 shadow-mapSize-height={2048}
             />
-            {/*outer walls:*/}
-            <Wall position={[0, 0, 5.5]} wallWidth={0.2} wallHeight={2.2} wallLength={11} props={""} />
-            <Wall position={[5, 0, 5.5]} wallWidth={0.2} wallHeight={2.2} wallLength={11} props={""} />
-            <Wall position={[1, 0, 11]} wallWidth={2} wallHeight={2.2} wallLength={0.2} props={""} />
-            <Wall position={[4, 0, 11]} wallWidth={2} wallHeight={2.2} wallLength={0.2} props={""} />
-            <Pillar position={[0, 0, 0]} />
-            <Wall position={[0.5, 0, 0]} wallWidth={1} wallHeight={2.2} wallLength={0.2} />
-            <Wall position={[3.5, 0, 0]} wallWidth={3} wallHeight={2.2} wallLength={0.2} />
-            <Window position={[1.5, 0, 0]} glassWidth={1} />
-            {/* inner walls groundFloor:*/}
-            <Window position={[0.5, 0, 8]} glassWidth={1} />
-            <Pillar position={[1, 0, 8]} />
-            <Window position={[1.5, 0, 8]} glassWidth={1} />
-            <Pillar position={[2, 0, 8]} />
-            <Window position={[2.5, 0, 8]} glassWidth={1} />
-            <Pillar position={[3, 0, 8]} />
-            <Window position={[3.5, 0, 8]} glassWidth={1} />
-            {/* stairs */}
+            {/* structures: :*/}
+            <OuterWallsGround />
+            <GlassWall />
             <Stairs />
         </>
     );
