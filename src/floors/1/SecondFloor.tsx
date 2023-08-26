5da69d2e58433b91de4f35de5cc1400b58634ebf
tzchrzanowski <tc157@student.london.ac.uk>
2023-08-27 07:41:42 +0800

    added more params to second floor windows and added more windows

--- src/floors/1/SecondFloor.tsx
+++ src/floors/1/SecondFloor.tsx
@@ -3,15 +3,28 @@ import OuterWallsFloor from "./structures/OuterWallsFloor";
 import GlassFloor from "./structures/GlassFloor";
 
 export default function SecondFloor () {
-    const northEastGlassWall: number = 7.1;
-    const southEastGlassWall: number = 2.9;
+    const zAxisSecondFloor: number = 2.1;
 
+    const xAxisNorth: number = 7.1;
+    const xAxisSouth: number = 2.9;
+
+    const yAxisEast: number = 1.2;
+    const yAxisWest: number = 4.4;
+
+    const eastWindowWidth: number = 2.4;
+    const westWindowWIdth: number = 0.9;
     return (
         <>
             <Floor_1 />
             <OuterWallsFloor />
-            <GlassFloor longerAxis={northEastGlassWall} />
-            <GlassFloor longerAxis={southEastGlassWall} />
+            {/* east windows */}
+            <GlassFloor zAxis={zAxisSecondFloor} yAxis={yAxisEast} longerAxis={xAxisNorth} windowWidth={eastWindowWidth}/>
+            <GlassFloor zAxis={zAxisSecondFloor} yAxis={yAxisEast} longerAxis={xAxisSouth} windowWidth={eastWindowWidth}/>
+
+            {/* west windows */}
+            <GlassFloor zAxis={zAxisSecondFloor} yAxis={yAxisWest} longerAxis={xAxisNorth} windowWidth={westWindowWIdth}/>
+            <GlassFloor zAxis={zAxisSecondFloor} yAxis={yAxisWest} longerAxis={xAxisSouth} windowWidth={westWindowWIdth}/>
+
         </>
     )
 }
--- src/floors/1/structures/GlassFloor.tsx
+++ src/floors/1/structures/GlassFloor.tsx
@@ -3,14 +3,20 @@ import Window from "../../../walls/Window";
 
 interface GlassFloorInterface {
     longerAxis: number,
+    zAxis: number,
+    yAxis: number,
+    windowWidth: number,
 }
 
-export default function GlassFloor ({longerAxis}: GlassFloorInterface) {
+export default function GlassFloor ({longerAxis, zAxis, yAxis, windowWidth}: GlassFloorInterface) {
+    const pillarPosOne = yAxis - (windowWidth / 2);
+    const pillarPosTwo = yAxis + (windowWidth / 2);
+
     return (
         <>
-            <Pillar position={[0.15, 2.1, longerAxis]} />
-            <Window position={[1.2, 2.1, longerAxis]} glassWidth={2.4} />
-            <Pillar position={[2.4, 2.1, longerAxis]} />
+            <Pillar position={[pillarPosOne, zAxis, longerAxis]} />
+            <Window position={[yAxis, zAxis, longerAxis]} glassWidth={windowWidth} />
+            <Pillar position={[pillarPosTwo, zAxis, longerAxis]} />
         </>
     )
 }
