c2ec6b5dbc08a3af1d1a92f1dd96d82de6ba58cf
tzchrzanowski <tc157@student.london.ac.uk>
2023-09-04 07:58:26 +0800

    prepared generic compound stairs generator component

--- src/components/compounds/Staris/Stairs.tsx
+++ src/components/compounds/Staris/Stairs.tsx
@@ -1,8 +1,47 @@
-import Wall from "../../../components/simple/Wall";
+import React from 'react';
+import Wall from "components/simple/Wall";
 
-export default function Stairs() {
+/*
+* Stairs component that is generated with small individual blocks of wall aligned together in pattern.
+* */
+
+interface StairsInterface {
+    stepsAmount: number,
+}
+
+export default function Stairs({stepsAmount}: StairsInterface) {
+    const [stepsList, setStepsList] = React.useState<number[]>([]);
+    const stepHeight = 0.16;
+    const stepDepth = 0.3;
+    const stairBlockHeight = 0.1;
+    const stairBlockWidth = 1;
+
+    React.useEffect(() => {
+        let newList = [];
+        for (let i = 0; i< stepsAmount; i++) {
+            newList.push(i);
+        }
+        setStepsList(newList);
+    }, []);
+
+    React.useEffect(()=> {
+        console.log(stepsList);
+    }, [stepsList])
+
+    // const renderStep = (id: number) => {
+    //
+    //     return (
+    //         <Wall position={[4.5, 1, 6.7]} wallWidth={1} wallHeight={0.1} wallLength={0.3} key={id}/>
+    //     )
+    // }
+
     return (
         <>
+            {/*{stepsList.map((step, id) => {*/}
+            {/*    return (*/}
+            {/*        <Wall position={[4.5, 1, 6.7]} wallWidth={1} wallHeight={0.1} wallLength={0.3} key={id}/>*/}
+            {/*    )*/}
+            {/*})}*/}
             <Wall position={[4.5, 1, 6.7]} wallWidth={1} wallHeight={0.1} wallLength={0.3} />
             <Wall position={[4.5, 0.84, 6.4]} wallWidth={1} wallHeight={0.1} wallLength={0.3} />
             <Wall position={[4.5, 0.68, 6.1]} wallWidth={1} wallHeight={0.1} wallLength={0.3} />
--- src/floors/0/GroundFloor.tsx
+++ src/floors/0/GroundFloor.tsx
@@ -1,14 +1,15 @@
-import Stairs from "./structures/Stairs";
+// import Stairs from "./structures/Stairs";
 import Floor_0 from "./structures/Floor_0";
 import OuterWalls from "components/compounds/outerWalls/outerWalls";
 import GlassWallSections from "components/compounds/glassWallSections/GlassWallSections";
+import Stairs from 'components/compounds/Staris/Stairs';
 
 export default function GroundFloor() {
     return (
         <>
             <OuterWalls zAxis={0} />
             <GlassWallSections zAxis={0} yAxis={8} startXAxis={0.0} glassWidth={1} numberOfElements={6}/>
-            <Stairs />
+            <Stairs stepsAmount={13} />
             <GlassWallSections zAxis={0} yAxis={2.8} startXAxis={0.0} glassWidth={1} numberOfElements={6}/>
             <Floor_0 />
         </>
