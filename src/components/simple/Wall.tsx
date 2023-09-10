4fc90e416d6858431813b36d632f13f526332b52
tzchrzanowski <tzchrzanowski@gmail.com>
2023-09-10 08:46:59 +0800

    added texture loading from url on wall

--- src/components/simple/Wall.tsx
+++ src/components/simple/Wall.tsx
@@ -1,5 +1,5 @@
-import { useRef, useState } from "react";
-
+import React, { useRef, useState } from "react";
+import { TextureLoader, Texture } from "three";
 // interface WallArgs {
 //     wallWidth: Number;
 //     wallLength: Number;
@@ -17,12 +17,21 @@ export default function Wall4m(props) {
     const [active, setActive ] = useState(false);
     const [transparentState, setTransparentState] = useState<boolean>(false);
     const [opacityState, setOpacityState] = useState<number>(0);
+    const [textureState, setTextureState] = useState<Texture>();
 
     const color = props.color || "lightGrey";
 
     const wallWidthFinal = props.wallWidth ? props.wallWidth : 0.2;
     const wallLengthFinal = props.wallLength ? props.wallLength : 0.2;
     const wallHeightFinal = props.wallHeight ? props.wallHeight : 2.2;
+
+    React.useEffect(()=> {
+        const loader = new TextureLoader();
+        const texture = loader.load('https://th.bing.com/th/id/OIP.ykkHrhIi-h54lF6g5EyLdwHaE9?pid=ImgDet&rs=1');
+        setTextureState(texture);
+        console.log("textureState: ", texture);
+    },[]);
+
 
     const clicked = () => {
         if(props.clickable == true){
@@ -46,7 +55,11 @@ export default function Wall4m(props) {
             opacity={opacityState}
         >
             <boxGeometry args={[wallWidthFinal, wallHeightFinal ,wallLengthFinal]} />
-            <meshStandardMaterial color={props.hoverable && hovered ? "cyan" : color} />
+            <meshStandardMaterial
+                // color={props.hoverable && hovered ? "cyan" : color}
+                // color={"0xFF8844"}
+                map={textureState}
+            />
         </mesh>
     )
 }
