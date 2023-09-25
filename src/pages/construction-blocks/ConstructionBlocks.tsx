a0ba42da38ac1ac0202884597586946c81ae795f
tzchrzanowski <tzchrzanowski@gmail.com>
2023-09-25 08:07:55 +0800

    added proper types to constructionElementParams

--- src/pages/construction-blocks/ConstructionBlocks.tsx
+++ src/pages/construction-blocks/ConstructionBlocks.tsx
@@ -1,14 +1,36 @@
 import React from 'react';
-import {Canvas} from "@react-three/fiber";
+import {Canvas, Vector3} from "@react-three/fiber";
 import {OrbitControls} from "@react-three/drei";
 import "./ConstructionBlocks.css";
 
 import {Scene} from "./Scene";
 import {TopNavigation} from "web-components/navigation/top-navigation/TopNavigation";
 import {Ground} from "three-components/simple/Ground";
+import {Elements} from "../../data/Elements";
 
-// TO-DO: set types for this list of objects
-const constructionElementsParams: any[] = [
+interface CoordsXY {
+    x: number,
+    y: number,
+}
+
+interface CoordsXYZ extends CoordsXY {
+    z: number,
+}
+
+interface Distance {
+    min: number,
+    max: number,
+}
+
+interface ElementParams {
+    elementName: keyof Elements,
+    position: Vector3,
+    lightPosition: CoordsXYZ,
+    groundPlatformDimensions: CoordsXY,
+    cameraDistanceParams: Distance,
+}
+
+const constructionElementsParams: ElementParams[] = [
     {
         elementName: "wall",
         position: [-1.5, 0.3, -8.8],
@@ -21,7 +43,7 @@ const constructionElementsParams: any[] = [
         position: [-1.5, -0.5, -2.5],
         lightPosition: {x: -10,z: 10,y: 15},
         groundPlatformDimensions: {x: 20, y: 20},
-        cameraDistanceParams: {min: 0, max: 12},
+        cameraDistanceParams: {min: 0, max: 6},
     },
     {
         elementName: "stairs",
